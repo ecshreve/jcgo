a369a0cdf93998a197800acbdd9bac355d540c91
Eric Shreve <ecshreve13@gmail.com>
2020-06-07 20:54:06 -0700

    doc: add comments

--- jcgo.go
+++ jcgo.go
@@ -13,16 +13,19 @@ import (
 func main() {
 	args := os.Args
 
+	// Require an argument.
 	if len(args) <= 1 {
 		log.Fatalf("please provide a file")
 	}
 
+	// Require that argument is a path to a .json file.
 	path := args[1]
 	ext := filepath.Ext(path)
 	if ext != ".json" {
 		log.Fatalf("please provide a .json file")
 	}
 
+	// Open the file.
 	file, err := os.Open(path)
 	if err != nil {
 		log.Fatal(err)
@@ -30,8 +33,10 @@ func main() {
 	log.Printf("successfully opened file %s\n", path)
 	defer file.Close()
 
+	// Read the file into a byte array.
 	byteValue, _ := ioutil.ReadAll(file)
 
+	// Unmarshall the byte array into a map.
 	var result map[string]interface{}
 	json.Unmarshal([]byte(byteValue), &result)
 
