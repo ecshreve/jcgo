5cbee673fdc874e62b5646de7722e5937f060fec
Eric Shreve <ecshreve13@gmail.com>
2020-06-14 02:39:30 -0700

    object: reorder functions

--- parser/object.go
+++ parser/object.go
@@ -22,59 +22,106 @@ func (objs ByPrefix) Swap(i, j int)      { objs[i], objs[j] = objs[j], objs[i] }
 // ObjectFromInterface returns an Object for the given input interface{}.
 func ObjectFromInterface(prefix string, input interface{}) Object {
 	switch vv := input.(type) {
+	case string:
+		return NewStringObj(prefix, vv)
+	case bool:
+		return NewBoolObj(prefix, vv)
+	case float64:
+		return NewFloatObj(prefix, vv)
 	case map[string]interface{}:
 		return NewMapObj(prefix, vv)
 	case []interface{}:
 		return NewSliceObj(prefix, vv)
-	case string:
-		return NewStringObj(prefix, vv)
-	case float64:
-		return NewFloatObj(prefix, vv)
-	case bool:
-		return NewBoolObj(prefix, vv)
 	default:
 		return nil
 	}
 }
 
-type SliceObj struct {
+// StringObj implements the Object interface for a string value.
+type StringObj struct {
 	Prefix string
-	Val    []Object
+	Val    string
 }
 
-// NewSliceObj returns a SliceObj for the given input slice.
-func NewSliceObj(prefix string, input []interface{}) *SliceObj {
-	var vals []Object
-
-	for _, v := range input {
-		vals = append(vals, ObjectFromInterface(prefix, v))
-	}
-
-	sort.Sort(ByPrefix(vals))
-
-	return &SliceObj{
+// NewStringObj returns a StringObj for the given input string.
+func NewStringObj(prefix string, input string) *StringObj {
+	return &StringObj{
 		Prefix: prefix,
-		Val:    vals,
+		Val:    input,
 	}
 }
 
-func (o SliceObj) getPrefix() string {
+func (o StringObj) getPrefix() string {
 	return o.Prefix
 }
 
-func (o SliceObj) Parse() [][]string {
-	var ret [][]string
+// Parse returns the 2d slice of strings for the given StringObj.
+func (o StringObj) Parse() [][]string {
+	return [][]string{
+		[]string{o.Prefix},
+		[]string{o.Val},
+	}
+}
 
-	for _, item := range o.Val {
-		parsed := item.Parse()
-		if len(ret) == 0 {
-			ret = append(ret, parsed[0])
-		}
+// BoolObj implements the Object interface for a float value.
+type BoolObj struct {
+	Prefix string
+	Val    bool
+}
 
-		ret = append(ret, parsed[1:]...)
+// NewBoolObj returns a BoolObj for the given input bool.
+func NewBoolObj(prefix string, input bool) *BoolObj {
+	return &BoolObj{
+		Prefix: prefix,
+		Val:    input,
+	}
+}
+
+func (o BoolObj) getPrefix() string {
+	return o.Prefix
+}
+
+// Parse returns the 2d slice of strings for the given BoolObj.
+func (o BoolObj) Parse() [][]string {
+	return [][]string{
+		[]string{o.Prefix},
+		[]string{strconv.FormatBool(o.Val)},
+	}
+}
+
+// FloatObj implements the Object interface for a float value.
+type FloatObj struct {
+	Prefix string
+	Val    float64
+}
+
+// NewFloatObj returns a FloatObj for the given input float.
+func NewFloatObj(prefix string, input float64) *FloatObj {
+	return &FloatObj{
+		Prefix: prefix,
+		Val:    input,
+	}
+}
+
+func (o FloatObj) getPrefix() string {
+	return o.Prefix
+}
+
+// Parse returns the 2d slice of strings for the given FloatObj.
+func (o FloatObj) Parse() [][]string {
+	floatVal := o.Val
+
+	var stringVal string
+	if float64(int64(floatVal)) == floatVal {
+		stringVal = strconv.FormatInt(int64(floatVal), 10)
+	} else {
+		stringVal = strconv.FormatFloat(floatVal, 'f', -1, 64)
 	}
 
-	return ret
+	return [][]string{
+		[]string{o.Prefix},
+		[]string{stringVal},
+	}
 }
 
 // MapObj implements the Object interface for a JSON map.
@@ -141,89 +188,44 @@ func (o MapObj) Parse() [][]string {
 	return ret
 }
 
-// StringObj implements the Object interface for a string value.
-type StringObj struct {
+// SliceObj implements the Object interface for a JSON array.
+type SliceObj struct {
 	Prefix string
-	Val    string
+	Val    []Object
 }
 
-// NewStringObj returns a StringObj for the given input string.
-func NewStringObj(prefix string, input string) *StringObj {
-	return &StringObj{
+// NewSliceObj returns a SliceObj for the given input slice.
+func NewSliceObj(prefix string, input []interface{}) *SliceObj {
+	var vals []Object
+
+	for _, v := range input {
+		vals = append(vals, ObjectFromInterface(prefix, v))
+	}
+
+	sort.Sort(ByPrefix(vals))
+
+	return &SliceObj{
 		Prefix: prefix,
-		Val:    input,
+		Val:    vals,
 	}
 }
 
-func (o StringObj) getPrefix() string {
+func (o SliceObj) getPrefix() string {
 	return o.Prefix
 }
 
-// Parse returns the 2d slice of strings for the given StringObj.
-func (o StringObj) Parse() [][]string {
-	return [][]string{
-		[]string{o.Prefix},
-		[]string{o.Val},
-	}
-}
+// Parse returns the 2d slice of strings for the given SliceObj.
+func (o SliceObj) Parse() [][]string {
+	var ret [][]string
 
-// FloatObj implements the Object interface for a float value.
-type FloatObj struct {
-	Prefix string
-	Val    float64
-}
+	for _, item := range o.Val {
+		parsed := item.Parse()
+		if len(ret) == 0 {
+			ret = append(ret, parsed[0])
+		}
 
-// NewFloatObj returns a FloatObj for the given input float.
-func NewFloatObj(prefix string, input float64) *FloatObj {
-	return &FloatObj{
-		Prefix: prefix,
-		Val:    input,
-	}
-}
-
-func (o FloatObj) getPrefix() string {
-	return o.Prefix
-}
-
-// Parse returns the 2d slice of strings for the given FloatObj.
-func (o FloatObj) Parse() [][]string {
-	floatVal := o.Val
-
-	var stringVal string
-	if float64(int64(floatVal)) == floatVal {
-		stringVal = strconv.FormatInt(int64(floatVal), 10)
-	} else {
-		stringVal = strconv.FormatFloat(floatVal, 'f', -1, 64)
+		ret = append(ret, parsed[1:]...)
 	}
 
-	return [][]string{
-		[]string{o.Prefix},
-		[]string{stringVal},
-	}
-}
-
-// BoolObj implements the Object interface for a float value.
-type BoolObj struct {
-	Prefix string
-	Val    bool
-}
-
-// NewBoolObj returns a BoolObj for the given input bool.
-func NewBoolObj(prefix string, input bool) *BoolObj {
-	return &BoolObj{
-		Prefix: prefix,
-		Val:    input,
-	}
-}
-
-func (o BoolObj) getPrefix() string {
-	return o.Prefix
-}
-
-// Parse returns the 2d slice of strings for the given BoolObj.
-func (o BoolObj) Parse() [][]string {
-	return [][]string{
-		[]string{o.Prefix},
-		[]string{strconv.FormatBool(o.Val)},
-	}
+	return ret
 }
