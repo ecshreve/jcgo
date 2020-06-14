12297c57a6bc08cc67416732bb225b468e85fde0
Eric Shreve <ecshreve13@gmail.com>
2020-06-14 07:46:42 -0700

    parser: add tests for file operations

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/davecgh/go-spew/spew"
+	"github.com/samsarahq/go/snapshotter"
 	"github.com/stretchr/testify/assert"
 
 	"github.com/ecshreve/jcgo/parser"
@@ -177,95 +178,38 @@ func TestTransform(t *testing.T) {
 	}
 }
 
-func TestParse(t *testing.T) {
-	data := testdata.NewObjectTestData()
-	spew.Config.DisablePointerAddresses = true
-	spew.Config.DisableCapacities = true
-	spew.Config.Indent = "  "
+func TestFileOperations(t *testing.T) {
+	snap := snapshotter.New(t)
+	defer snap.Verify()
 
-	testcases := []struct {
-		description string
-		input       parser.Object
-		expected    [][]string
-	}{
-		{
-			description: "simple string",
-			input: &parser.StringObj{
-				Prefix: "key1",
-				Val:    "val1",
-			},
-			expected: [][]string{
-				[]string{"key1"},
-				[]string{"val1"},
-			},
-		},
-		{
-			description: "simple map",
-			input:       data.SimpleMapObj,
-			expected: [][]string{
-				[]string{"key1", "key2", "key3"},
-				[]string{"val1", "val2", "val3"},
-			},
-		},
-		{
-			description: "simple map with all possible data types",
-			input:       data.SimpleAllTypesMapObj,
-			expected: [][]string{
-				[]string{"key1", "key2", "key3", "key4"},
-				[]string{"val1", "5", "5.5", "true"},
-			},
-		},
-		{
-			description: "simple nested map",
-			input:       data.SimpleNestedMapObj,
-			expected: [][]string{
-				[]string{"outer1_inner1", "outer1_inner2", "outer2"},
-				[]string{"innerval1", "innerval2", "outerval2"},
-			},
-		},
-		{
-			description: "double nested map",
-			input:       data.DoubleNestedMapObj,
-			expected: [][]string{
-				[]string{"outer1_inner1", "outer1_nestedmap_nested1", "outer1_nestedmap_nested2", "outer2"},
-				[]string{"innerval1", "nestedval1", "nestedval2", "outerval2"},
-			},
-		},
-		{
-			description: "map with simple slice",
-			input:       data.SimpleSliceMapObj,
-			expected: [][]string{
-				[]string{"data_key1", "data_key2", "data_key3"},
-				[]string{"val1", "val2", "val3"},
-				[]string{"val4", "val5", "val6"},
-			},
-		},
-		{
-			description: "map with slice with nested map",
-			input:       data.SimpleSliceNestedMapObj,
-			expected: [][]string{
-				[]string{"data_key1", "data_nestedmap_nested1", "data_nestedmap_nested2"},
-				[]string{"val1", "nestedval1", "nestedval2"},
-				[]string{"val4", "nestedval3", "nestedval4"},
-			},
-		},
-		{
-			description: "complex map with nested maps and slices",
-			input:       data.ComplexMapObj,
-			expected: [][]string{
-				[]string{"data_key1", "data_nestedmap_nested1", "data_nestedmap_nested2", "data_nestedslice_after_nestedslicemap1", "data_nestedslice_after_nestedslicemap2", "data_nestedslice_before_nestedslicemap1", "data_nestedslice_before_nestedslicemap2"},
-				[]string{"val1", "nestedval1", "nestedval2", "nestedslicemapval5", "nestedslicemapval6", "nestedslicemapval1", "nestedslicemapval2"},
-				[]string{"val1", "nestedval1", "nestedval2", "nestedslicemapval7", "nestedslicemapval8", "nestedslicemapval3", "nestedslicemapval4"},
-				[]string{"val1", "nestedval1", "nestedval2", "nestedslicemapval15", "nestedslicemapval16", "nestedslicemapval11", "nestedslicemapval12"},
-				[]string{"val1", "nestedval1", "nestedval2", "nestedslicemapval17", "nestedslicemapval18", "nestedslicemapval13", "nestedslicemapval14"},
-			},
-		},
-	}
+	// Verify we can read a valid json file.
+	raw, err := parser.ReadJSONFile("testdata/jsontest.json")
+	assert.NoError(t, err)
+	assert.NotNil(t, raw)
+	snap.Snapshot("read valid json file", raw)
 
-	for _, testcase := range testcases {
-		t.Run(testcase.description, func(t *testing.T) {
-			actual, _ := testcase.input.Parse()
-			assert.Equal(t, testcase.expected, actual)
-		})
+	// Expect error if file doesn't exist.
+	raw, err = parser.ReadJSONFile("testdata/nonexistentfile.json")
+	assert.Error(t, err)
+	assert.Nil(t, raw)
+
+	// Expect error if file is malformed.
+	raw, err = parser.ReadJSONFile("testdata/jsontest_bad.json")
+	assert.Error(t, err)
+	assert.Nil(t, raw)
+
+	// Verify we can write valid data to CSV.
+	data := [][]string{
+		[]string{"one", "two"},
+		[]string{"one_one", "two_two"},
 	}
+	file, err := parser.WriteCSVFile(data, "testdata/testoutput.csv")
+	assert.NoError(t, err)
+	assert.NotNil(t, file)
+
+	// Expect error for a bad path.
+	file, err = parser.WriteCSVFile(data, "testdata/nonexistentdirectory/testoutput.xls")
+	assert.Error(t, err)
+	assert.Nil(t, file)
+
 }
