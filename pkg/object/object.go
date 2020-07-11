3dc9e0668c2593f641c5266ce02f4026be96f700
Eric Shreve <ecshreve13@gmail.com>
2020-07-10 21:26:19 -0700

    pkg object: transfer some object functions to pkg/
    Planning to expose a public api so this package can get imported and
    used in other places. Eventually I'll remove the internal/ directory
    completely.

---
+++ pkg/object/object.go
@@ -0,0 +1,134 @@
+// Package object provides implementations of the Object interface for the
+// possible JSON input value types.
+package object
+
+import (
+	"strconv"
+
+	"github.com/samsarahq/go/oops"
+)
+
+// Prefix is just a string, we redefine it so we can implement the getPrefix
+// method of the Object interface once rather than for each new Object type.
+type Prefix string
+
+func (p Prefix) getPrefix() string {
+	return string(p)
+}
+
+// NewPrefix returns a pointer to a Prefix for the given string.
+func NewPrefix(p string) *Prefix {
+	pp := Prefix(p)
+	return &pp
+}
+
+// Object is representation of a JSON object.
+type Object interface {
+	getPrefix() string
+	Parse() ([][]string, error)
+}
+
+// ByPrefix implements the sort.Interface on the Prefix field.
+type ByPrefix []Object
+
+func (objs ByPrefix) Len() int           { return len(objs) }
+func (objs ByPrefix) Less(i, j int) bool { return objs[i].getPrefix() < objs[j].getPrefix() }
+func (objs ByPrefix) Swap(i, j int)      { objs[i], objs[j] = objs[j], objs[i] }
+
+// FromInterface returns the Object for the given input interface and returns an
+// error if the interface is of an invalid type.
+func FromInterface(prefix string, input interface{}) (Object, error) {
+	switch vv := input.(type) {
+	case nil:
+		return NewStringObj(prefix, ""), nil
+	case string:
+		return NewStringObj(prefix, vv), nil
+	case bool:
+		return NewBoolObj(prefix, vv), nil
+	case float64:
+		return NewNumberObj(prefix, vv), nil
+	default:
+		return nil, oops.Errorf("unable to create Object from interface: %+v", input)
+	}
+}
+
+// StringObj implements the Object interface for a string value.
+type StringObj struct {
+	*Prefix
+	Val string
+}
+
+// NewStringObj returns a StringObj for the given input string.
+func NewStringObj(prefix string, input string) *StringObj {
+	return &StringObj{
+		NewPrefix(prefix),
+		input,
+	}
+}
+
+// Parse returns the 2d slice of strings for the given StringObj.
+func (o StringObj) Parse() ([][]string, error) {
+	return [][]string{
+		{string(*o.Prefix)},
+		{o.Val},
+	}, nil
+}
+
+// BoolObj implements the Object interface for a bool value.
+type BoolObj struct {
+	*Prefix
+	Val bool
+}
+
+// NewBoolObj returns a BoolObj for the given input bool.
+func NewBoolObj(prefix string, input bool) *BoolObj {
+	return &BoolObj{
+		NewPrefix(prefix),
+		input,
+	}
+}
+
+// Parse returns the 2d slice of strings for the given BoolObj.
+func (o BoolObj) Parse() ([][]string, error) {
+	return [][]string{
+		{string(*o.Prefix)},
+		{strconv.FormatBool(o.Val)},
+	}, nil
+}
+
+// NumberObj implements the Object interface for a numeric value.
+type NumberObj struct {
+	*Prefix
+	Val float64
+}
+
+// NewNumberObj returns a NumberObj for the given input float.
+func NewNumberObj(prefix string, input float64) *NumberObj {
+	return &NumberObj{
+		NewPrefix(prefix),
+		input,
+	}
+}
+
+// Parse returns the 2d slice of strings for the given FloatObj.
+//
+// We do a check to see if the value is actually an integer, and treat if so.
+// This check is necessary because the results of unmarshalling a JSON byte
+// array into a map[string]interface{} treats all numeric values as floats. We
+// want the string representation that we eventually write to a CSV file to have
+// numeric values appear accurately if they're integers. i.e. "345" not "345.0".
+func (o NumberObj) Parse() ([][]string, error) {
+	floatVal := o.Val
+
+	var stringVal string
+	if float64(int64(floatVal)) == floatVal {
+		stringVal = strconv.FormatInt(int64(floatVal), 10)
+	} else {
+		stringVal = strconv.FormatFloat(floatVal, 'f', -1, 64)
+	}
+
+	return [][]string{
+		{string(*o.Prefix)},
+		{stringVal},
+	}, nil
+}
---
+++ pkg/object/object_test.go
@@ -0,0 +1,103 @@
+package object_test
+
+import (
+	"testing"
+
+	"github.com/samsarahq/go/snapshotter"
+	"github.com/stretchr/testify/assert"
+
+	oo "github.com/ecshreve/jcgo/pkg/object"
+)
+
+func TestObjectCreation(t *testing.T) {
+	snap := snapshotter.New(t)
+	defer snap.Verify()
+
+	testcases := []struct {
+		description string
+		input       interface{}
+		expectError bool
+	}{
+		{
+			description: "create nil StringObj",
+			input:       nil,
+			expectError: false,
+		},
+		{
+			description: "create StringObj",
+			input:       "this is a string",
+			expectError: false,
+		},
+		{
+			description: "create BoolObj",
+			input:       true,
+			expectError: false,
+		},
+		{
+			description: "create NumberObj",
+			input:       float64(10.0),
+			expectError: false,
+		},
+		{
+			description: "invalid scalar input",
+			input:       int64(1),
+			expectError: true,
+		},
+	}
+
+	for _, testcase := range testcases {
+		t.Run(testcase.description, func(t *testing.T) {
+			obj, err := oo.FromInterface("", testcase.input)
+			assert.Equal(t, testcase.expectError, err != nil)
+			snap.Snapshot(testcase.description, obj)
+		})
+	}
+}
+
+func TestParseObject(t *testing.T) {
+	testcases := []struct {
+		description string
+		input       oo.Object
+		expected    [][]string
+	}{
+		{
+			description: "simple string",
+			input:       oo.NewStringObj("pref1", "stringVal"),
+			expected: [][]string{
+				{"pref1"},
+				{"stringVal"},
+			},
+		},
+		{
+			description: "simple bool",
+			input:       oo.NewBoolObj("pref1", true),
+			expected: [][]string{
+				{"pref1"},
+				{"true"},
+			},
+		},
+		{
+			description: "simple float number",
+			input:       oo.NewNumberObj("pref1", 5.5),
+			expected: [][]string{
+				{"pref1"},
+				{"5.5"},
+			},
+		},
+		{
+			description: "simple integer number",
+			input:       oo.NewNumberObj("pref1", 5.0),
+			expected: [][]string{
+				{"pref1"},
+				{"5"},
+			},
+		},
+	}
+
+	for _, testcase := range testcases {
+		t.Run(testcase.description, func(t *testing.T) {
+			actual, _ := testcase.input.Parse()
+			assert.Equal(t, testcase.expected, actual)
+		})
+	}
+}
